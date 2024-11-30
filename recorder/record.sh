855282114c0180d613df84ee8d57388124d62fc2
Q <Q>
2024-11-30 22:59:25 +0800

    month dir

--- recorder/record.sh
+++ recorder/record.sh
@@ -16,17 +16,29 @@ TIMEOUT=$(( $DURATION + $TIMEOUT_BUFFER ))
 
 while true
 do
-echo "started at $(date)"
-echo "should timeout after $DURATION"
-timeout --kill-after $TIMEOUT_BUFFER -v $DURATION \
-ffmpeg -nostdin -loglevel $LOG_LEVEL \
--y -i $RTSP_URL -timeout 60000000 \
--vsync 1 -async -1 -an -vcodec copy \
--t $DURATION \
--f segment -strftime 1 -segment_time $DURATION \
--segment_format $FORMAT "/out/${NAME}/${SEGMENT_FORMAT}_${NAME}.$FORMAT" \
--reset_timestamps 1 -segment_atclocktime 1 
+  echo "started at $(date)"
+  echo "should timeout after $DURATION"
 
-echo "stopped at $(date)"
-sleep 2;
+  # 获取当前日期和月份
+  YEAR_MONTH=$(date +%Y-%m)
+  DAY_TIME=$(date +%d_%H%M%S)
+
+  # 创建月份文件夹
+  MONTH_DIR="/out/${NAME}/${YEAR_MONTH}"
+  if [ ! -e $MONTH_DIR ]; then
+    mkdir -p $MONTH_DIR
+  fi
+  chmod 775 $MONTH_DIR
+
+  timeout --kill-after $TIMEOUT_BUFFER -v $DURATION \
+  ffmpeg -nostdin -loglevel $LOG_LEVEL \
+  -y -i $RTSP_URL -timeout 60000000 \
+  -vsync 1 -async -1 -an -vcodec copy \
+  -t $DURATION \
+  -f segment -strftime 1 -segment_time $DURATION \
+  -segment_format $FORMAT "${MONTH_DIR}/${DAY_TIME}_${NAME}.$FORMAT" \
+  -reset_timestamps 1 -segment_atclocktime 1 
+
+  echo "stopped at $(date)"
+  sleep 2;
 done
